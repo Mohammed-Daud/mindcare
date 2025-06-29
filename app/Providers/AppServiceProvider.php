778c8fa2558db1fd77da4bc4028bb8de195f4a6c
daud-mis <devops.24miswork@gmail.com>
2025-06-29 12:30:23 +0530

    fix migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Force HTTPS in production, but not for ngrok URLs in local environment
-        if (config('app.env') === 'production' || 
-            (config('app.env') === 'local' && !str_contains(request()->getHost(), 'ngrok-free.app'))) {
-            URL::forceScheme('https');
-        }
+        // if (config('app.env') === 'production' || 
+        //     (config('app.env') === 'local' && !str_contains(request()->getHost(), 'ngrok-free.app'))) {
+        //     URL::forceScheme('https');
+        // }
+        Schema::defaultStringLength(191);
     }
 }
